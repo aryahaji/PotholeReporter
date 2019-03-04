bc512f1a8890acb24af71a56208a1806ccaf06ad
Arya Hajikandi <aryahaji@buffalo.edu>
2019-03-04 10:22:43 -0500

    added necessary imports

--- PotHoleReporter/__init__.py
+++ PotHoleReporter/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 application.config.from_pyfile('config.py')
 
 application.config['SECRET_KEY']
+application.config['SQLALCHEMY_DATABASE_URI']
+db = SQLAlchemy(application)
 
 from PotHoleReporter import routes
--- PotHoleReporter/models.py
+++ PotHoleReporter/models.py
@@ -1,3 +1,6 @@
+from PotHoleReporter import db
+from flask_login import UserMixin
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True, nullable = False)
