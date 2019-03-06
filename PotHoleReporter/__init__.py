c07c6a16a54d8193b40df8bbba0ad3db7cb8b30f
Kyle Skompinski <kyleskom@buffalo.edu>
2019-03-06 01:13:49 -0500

    Created database models and set up db to be used with web app (#11)

--- PotHoleReporter/__init__.py
+++ PotHoleReporter/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__, instance_relative_config=True)
 application.config.from_pyfile('config.py')
 application.config['SECRET_KEY']
+application.config['SQLALCHEMY_DATABASE_URI']
+db = SQLAlchemy(application)
