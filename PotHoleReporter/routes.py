392f62d64eeedf552f6b96a5137cc8f0bbf0e6fd
Arya Hajikandi <aryahaji@buffalo.edu>
2019-03-06 01:31:23 -0500

    added functionality for administrative user to create and login to an account. Also fixed missing backslashes for login, register, and logout routes. Finally added some missing compenents in the layout.html file in order for it to display our content properly

--- PotHoleReporter/routes.py
+++ PotHoleReporter/routes.py
@@ -1,5 +1,8 @@
-from flask import render_template, url_for, redirect
-from PotHoleReporter import application
+from flask import render_template, url_for, redirect, flash, request
+from PotHoleReporter import application, db, bcrypt
+from PotHoleReporter.forms import LoginForm, RegisterForm
+from PotHoleReporter.models import User, Towns, Tickets
+from flask_login import login_user, current_user, login_required, logout_user
 
 @application.route('/')
 @application.route('/home')
@@ -16,13 +19,32 @@ def contact():
 
 @application.route('login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(town=form.town.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            nextPage = request.args.get('next')
+            return redirect(nextPage) if nextPage else redirect(url_for('home'))
+
+    return render_template('login.html', title='Login', form=form)
 
 @application.route('logout')
 def logout():
+    logout_user()
     return redirect(url_for('login'))
 
 @application.route('register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(firstName=form.firstName.data, lastName=form.lastName.data, town=form.town.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
