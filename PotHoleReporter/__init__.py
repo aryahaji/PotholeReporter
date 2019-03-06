429ac56d939406c570ee1f2fa488cff6dabf9d5e
KyleWilliamz <kjw22@buffalo.edu>
2019-03-06 01:00:48 -0500

    Added application config information

--- PotHoleReporter/__init__.py
+++ PotHoleReporter/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
 
-application = Flask(__name__)
-
+application = Flask(__name__, instance_relative_config=True)
+application.config.from_pyfile('config.py')
+application.config['SECRET_KEY']
