d0f346a94fe7a129b59a597ad8d8702a5726f3c0
Arya Hajikandi <aryahaji@buffalo.edu>
2019-03-31 19:49:12 -0400

    created a create ticket form and fixed some files that didnt end with endblock content

--- PotHoleReporter/forms.py
+++ PotHoleReporter/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegisterForm(FlaskForm):
@@ -15,3 +15,12 @@ class LoginForm(FlaskForm):
     town = StringField('Town', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login!')
+
+class SubmitTicketForm(FlaskForm):
+    towns = [('1', 'Buffalo'), ('2', 'Amherst'), ('3', 'Clarence'), ('4', 'West Seneca'), ('5', 'Cheektowaga'), ('6', 'Tonawanda'), ('7', 'Eden'),
+    ('8', 'Grand Island'), ('9', 'Lancaster'), ('10', 'Williamsville'), ('11', 'Hamburg'), ('12', 'Orchard Park'), ('13', 'Depew'), ('14', 'Kenmore'), ('15', 'Angola')]
+    town = SelectField('Town', choices=towns , validators=[DataRequired()])
+    size = RadioField('Size', choices=[('s', 'Small'), ('m', 'Medium'), ('l', 'Large')], validators=[DataRequired()])
+    xcord = StringField('x-Coordinate', validators=[DataRequired()])
+    ycord = StringField('y-Coordinate', validators=[DataRequired()])
+    submit = SubmitField('Create Ticket!')
